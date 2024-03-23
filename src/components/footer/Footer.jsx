be0a98927395ccdbd20279d427084ede057e80a4
VjOnWeb <98512909+VjOnWeb@users.noreply.github.com>
2024-03-23 19:23:00 +0530

    added Footer and testimonals

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import './footer.css'
+// Footer.js
+import React from 'react';
+import './footer.css';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-sections">
+          <h3>Sections</h3>
+          <ul>
+            <li><a href="#about">About</a></li>
+            <li><a href="#experience">Experience</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#portfolio">Portfolio</a></li>
+            <li><a href="#testimonials">Testimonials</a></li>
+          </ul>
+        </div>
+        <div className="footer-social">
+          <h3>Follow Us</h3>
+          <ul>
+            <li><a href="https://facebook.com">Facebook</a></li>
+            <li><a href="https://twitter.com">Twitter</a></li>
+            <li><a href="https://instagram.com">Instagram</a></li>
+            {/* Add more social links as needed */}
+          </ul>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <p>&copy; 2024 Your Website. All Rights Reserved.</p>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-import './testimonials.css'
+// Testimonials.jsx
+import React from 'react';
+import './testimonials.css';
+
 const Testimonials = () => {
   return (
-    <section id='testimonals'>Testimonials</section>
-  )
+    <section id='testimonials' className='testimonials-section'>
+      <div className='container'>
+        <h2>Testimonials</h2>
+        <div className='testimonial'>
+          <div className='testimonial-content'>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum leo ut purus consectetur, ac elementum odio cursus.</p>
+          </div>
+          <div className='testimonial-author'>
+            <img src='https://via.placeholder.com/150' alt='Author' />
+            <div>
+              <h4>John Doe</h4>
+              <p>CEO, Company Name</p>
+            </div>
+          </div>
+        </div>
+        {/* Add more testimonials as needed */}
+      </div>
+    </section>
+  );
 }
 
-export default Testimonials
+export default Testimonials;
