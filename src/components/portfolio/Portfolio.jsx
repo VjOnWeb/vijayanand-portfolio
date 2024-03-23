744984e0477c44cd569a881e332221d614cf1426
VjOnWeb <98512909+VjOnWeb@users.noreply.github.com>
2024-03-23 13:38:20 +0530

    UI changes

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,27 +1,27 @@
 import React from 'react'
-import './portfolio.css'
 import IMG1 from '../../assets/portfolio1.jpg'
 import IMG2 from '../../assets/portfolio2.jpg'
 import IMG3 from '../../assets/portfolio3.jpg'
+import './portfolio.css'
 
 const data = [
   {
     id: 1,
     image: IMG1,
     title: 'Hotel Booking Management System',
-    github: 'https://github.com/vinothaselvam'
+    github: 'https://github.com/VjOnWeb/'
   },
   {
     id: 2,
     image: IMG2,
     title: 'Flower Shop',
-    github: 'https://github.com/vinothaselvam'
+    github: 'https://github.com/VjOnWeb/'
   },
   {
     id: 3,
     image: IMG3,
     title: 'Personal Portfolio',
-    github: 'https://github.com/vinothaselvam'
+    github: 'https://github.com/VjOnWeb/'
   }
 ]
 
