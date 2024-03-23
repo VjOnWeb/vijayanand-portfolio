a6d6682e57e167b5c78de1b75f583f6fab8024ae
VjOnWeb <98512909+VjOnWeb@users.noreply.github.com>
2024-03-23 18:44:05 +0530

    Contact Changes

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -23,14 +23,14 @@ const contact = () => {
           <article className="contact__option">
             <RiMessengerLine className='contact__option-icon'/>
             <h4>Messenger</h4>
-            <h5>Vinotha</h5>
+            <h5>Vijay Anand</h5>
             <a href="mailto:vijayanandvj1998@gmail.com" target="_blank">Send a message</a>
           </article>
 
           <article className="contact__option">
             <BsWhatsapp className='contact__option-icon'/>
             <h4>WhatsApp</h4>
-            <h5>+91-8838301883</h5>
+            <h5><a href="tel:+918838301883">+91 8838301883</a></h5>
             <a href="https://wa.me/918838301883" target="_blank">Send a message</a>
           </article>
 
