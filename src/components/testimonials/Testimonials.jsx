8f80d5767ea70e1a5fadacd82964ad3ae5d63fd5
VjOnWeb <98512909+VjOnWeb@users.noreply.github.com>
2024-03-23 11:56:51 +0530

    Updates

--- src/components/testimonials/Testimonials.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './testimonials.css'
-const Testimonials = () => {
-  return (
-    <section id='testimonals'>Testimonials</section>
-  )
-}
-
-export default Testimonials
